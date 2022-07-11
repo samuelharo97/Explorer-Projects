363a3d74b186f11b2b2e975e088becea21963e2c
Samuel Haro <samuel.hm@hotmail.com>
2022-07-11 05:17:57 -0300

    commit 2

--- SPA-Universe/js/index.js
+++ SPA-Universe/js/index.js
@@ -2,7 +2,7 @@ import { Router } from './router.js'
 
 const router = new Router()
 router.add('/', '/pages/home.html')
-router.add('/about', '/pages/about.html')
+router.add('/universo', '/pages/universo.html')
 router.add('/contact', '/pages/contact.html')
 router.add(404, '/pages/404.html')
 
@@ -10,4 +10,3 @@ router.handle()
 
 window.onpopstate = () => router.handle()
 window.route = () => router.route()
-

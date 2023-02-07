d62b4ebbb9b1b382e7ec10cc8276949c28363755
Samuel Haro <samuel.hm@hotmail.com>
2023-02-07 03:22:12 -0300

    finished raw version

--- gitfav/main.js
+++ gitfav/main.js
@@ -1,3 +1,7 @@
+const button = document.getElementById('add')
+const search = document.getElementById('input-search')
+const table = document.getElementById('favorites')
+
 let favorites = new Array()
 
 function loadData() {
@@ -24,9 +28,8 @@ function render() {
         </td>
         <td>${user.repos}</td>
         <td>${user.followers}</td>  
-        <td class="remove">Remover</td>
+        <td> <button id=${user.id} class="remove"> Remover </button></td>
         </td>
-   
        `
     table.appendChild(tr)
   })
@@ -35,40 +38,52 @@ function render() {
 loadData()
 
 function getData(user) {
-  try {
-    const data = fetch(`https://api.github.com/users/${user}`)
-      .then(res => res.json())
-      .then(data => data)
-      .catch(err => console.error(err))
-    return data
-  } catch (error) {
-    console.log(error)
-  }
+  return new Promise(async (resolve, reject) => {
+    try {
+      const data = await fetch(`https://api.github.com/users/${user}`)
+        .then(res => res.json())
+        .then(data => data)
+      resolve(data)
+    } catch (error) {
+      reject(console.error(error))
+    }
+  })
 }
 
 function saveData(user) {
+  removeAll()
   favorites.push(user)
   localStorage.clear()
   localStorage.setItem('@GitFavorites:', JSON.stringify(favorites))
-  removeAll()
   render()
 }
 
 function removeAll() {
-  const allUsers = document.body.querySelectorAll('.user')
-  allUsers.forEach(element => {
-    element.remove()
+  favorites.forEach(user => {
+    const element = document.getElementById(user.id)
+    if (element) {
+      element.remove()
+    }
   })
 }
 
-async function create(data) {
-  const data = await getData()
-  const user = new GitFavorite(data)
+function deleteUser(userId) {
+  removeAll()
+  const updatedFavorites = favorites.filter(user => user.id !== userId)
+  localStorage.clear()
+  localStorage.setItem('@GitFavorites:', JSON.stringify(updatedFavorites))
+  loadData()
+}
+
+async function create(value) {
+  const info = await getData(value)
+  const user = new GitFavorite(info)
+  return user
 }
 
 export class GitFavorite {
   constructor(data) {
-    this.id = data.id
+    this.id = this.generateRandomID()
     this.name = data.name
     this.username = data.login
     this.avatar = data.avatar_url
@@ -76,4 +91,25 @@ export class GitFavorite {
     this.followers = data.followers
     this.url = data.url
   }
+
+  generateRandomID() {
+    return Math.random().toString(36)
+  }
 }
+
+button.addEventListener('click', async () => {
+  if (search.value) {
+    const user = await create(search.value)
+    if (user) {
+      saveData(user)
+    }
+  }
+})
+
+table.addEventListener('click', event => {
+  if (event.target.classList == 'remove') {
+    confirm('Gostaria de remover este usuário?')
+      ? deleteUser(event.target.id)
+      : null
+  }
+})

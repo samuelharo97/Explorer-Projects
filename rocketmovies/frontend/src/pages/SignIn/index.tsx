b2a671828bdf0a128239674f6e37c8062a2ad2f7
Samuel Haro <samuel.hm@hotmail.com>
2023-03-05 23:01:36 -0300

    adds login function to signIn page

--- rocketmovies/frontend/src/pages/SignIn/index.tsx
+++ rocketmovies/frontend/src/pages/SignIn/index.tsx
@@ -1,24 +1,72 @@
 import { InputText, Button, LinkText, LandingLogo } from '@components';
 import { AuthContext } from '@context';
-import { useContext } from 'react';
+import { api } from '@service';
+import { useContext, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
 import { AiOutlineMail, AiOutlineLock } from 'react-icons/ai';
 
+type FormData = {
+  email: string;
+  password: string;
+};
 
 export const SignIn = () => {
   const [auth, setAuth] = useContext(AuthContext);
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    setError,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  useEffect(() => {
+    register('email', { required: true });
+    register('password', { required: true });
+  }, []);
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+    try {
+      api.post('/login', data).then((res) => {
+        console.log(res);
+        setAuth(true);
+      });
+    } catch (error) {
+      alert(error);
+      console.log(error);
+    }
+  };
 
   return (
     <div className="flex h-screen items-center self-center">
       <section className="md:flex-col md:w-4/12 my-64 mx-auto md:self-center">
         <LandingLogo welcomeText="Faça seu login" />
-        <div className="flex-col justify-center w-fit">
-          <InputText placeholder="E-mail" icon={AiOutlineMail} inputType="email"></InputText>
+        <form onSubmit={handleSubmit(onSubmit)} className="flex-col justify-center w-fit">
+          <InputText
+            placeholder="E-mail"
+            icon={AiOutlineMail}
+            inputType="email"
+            inputName="email"
+            func={(e: any) => {
+              setValue('email', e.target.value);
+            }}
+          ></InputText>
 
-          <InputText placeholder="Senha" inputType="password" icon={AiOutlineLock}></InputText>
+          <InputText
+            placeholder="Senha"
+            inputType="password"
+            inputName="password"
+            func={(e: any) => {
+              setValue('password', e.target.value);
+            }}
+            icon={AiOutlineLock}
+          ></InputText>
 
-          <Button text="Entrar" onClick={() => setAuth(true)} color="bg-rose-400"></Button>
+          <Button text="Entrar" buttonType="submit" color="bg-rose-400"></Button>
           <LinkText goTo={'/register'} text="Criar conta" />
-        </div>
+        </form>
       </section>
       <section className="h-screen w-6/12 hidden md:w-7/12 md:ml-10 lg:block">
         <img
